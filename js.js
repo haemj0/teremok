eb813d3587736b9e80a3bdb367e892646db5c212
haemj0 <161687472+haemj0@users.noreply.github.com>
2025-01-19 17:40:49 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,80 @@
+var clicks = 0;
+  function onClick() {
+  clicks += 1;
+  document.getElementById("clicks").innerHTML = clicks;
+  if (clicks>=6){
+    document.getElementById("str").style.display = 'block'
+  }
+};
+
+var click = 0;
+  function onClickk() {
+  click += 1;
+  document.getElementById("click").innerHTML = click;
+};
+
+
+
+
+const el1 = document.body.querySelector('.volk')
+
+el1.addEventListener('click', () => {
+  el1.className = 'volkk'
+
+})
+
+
+const el2 = document.body.querySelector('.rabbit')
+
+el2.addEventListener('click', () => {
+  el2.className = 'rabbitt'
+
+})
+
+const el3 = document.body.querySelector('.cat')
+
+el3.addEventListener('click', () => {
+  el3.className = 'element-final'
+})
+
+const el4 = document.body.querySelector('.lisa')
+
+el4.addEventListener('click', () => {
+  el4.className = 'lisaa'
+
+})
+
+const el5 = document.body.querySelector('.petuh')
+
+el5.addEventListener('click', () => {
+  el5.className = 'element-final'
+
+})
+
+const el6 = document.body.querySelector('.mouse')
+
+el6.addEventListener('click', () => {
+  el6.className = 'mousee'
+
+})
+
+const el7 = document.body.querySelector('.pig')
+
+el7.addEventListener('click', () => {
+  el7.className = 'hh'
+
+})
+
+const el8 = document.body.querySelector('.frog')
+
+el8.addEventListener('click', () => {
+  el8.className = 'frogg'
+
+})
+
+const el9 = document.body.querySelector('.beer')
+
+el9.addEventListener('click', () => {
+  el9.className = 'beeer'
+
+})
